adb9b2ef34febba9bbfec39139dc372b8196d7ef
Pawel <pawelpiatek98@gmail.com>
2022-03-13 11:54:41 +0100

    added facial recognition

--- main.py
+++ main.py
@@ -4,27 +4,41 @@ import cv2
 app = Flask(__name__)
 camera = cv2.VideoCapture(0)
 
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+ds_factor = 0.6
+
+
+def get_frames():
+    ret, frame = camera.read()
+    frame = cv2.resize(frame, None, fx=ds_factor, fy=ds_factor,
+                       interpolation=cv2.INTER_AREA)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face_rects = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in face_rects:
+        print('face')
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        break
+    ret, jpeg = cv2.imencode('.jpg', frame)
+    return jpeg.tobytes()
+
 
 def generate_frames():
     while True:
-
-        success, frame = camera.read()
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
+        frame = get_frames()
 
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/video')
 def video():
-    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run(debug=True)
